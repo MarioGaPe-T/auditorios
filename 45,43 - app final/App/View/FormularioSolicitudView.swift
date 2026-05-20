49f7d0a4123c9f6ef4d52ac419531e33198a8786
MarioGaPe-T <l22270445@tuxtla.tecnm.mx>
2026-05-20 15:20:31 -0600

    Final final feliz

--- 45,43 - app final/App/View/FormularioSolicitudView.swift
+++ 45,43 - app final/App/View/FormularioSolicitudView.swift
@@ -2,11 +2,6 @@
 //  FormularioSolicitudView.swift
 //  45,43 - app final
 //
-//  Created by Alumno on 13/05/26.
-//
-
-
-
 
 import SwiftUI
 
@@ -15,29 +10,12 @@ struct FormularioSolicitudView: View {
     let sala: String
     let bloquesSeleccionados: [BloqueHorario]
     let accionCancelar: () -> Void
-    let accionEnviar: (
-        String,
-        Bool,
-        Bool,
-        Bool
-    ) -> Void
+    let accionEnviar: (String, Bool, Bool, Bool) -> Void
 
     @State private var motivo = ""
     @State private var necesitaMicrofono = false
     @State private var necesitaBocina = false
     @State private var necesitaProyector = false
-
-    private var fecha: String {
-        bloquesSeleccionados.first?.fecha ?? ""
-    }
-
-    private var horaInicio: String {
-        bloquesSeleccionados.first?.horaInicio ?? ""
-    }
-
-    private var horaFin: String {
-        bloquesSeleccionados.last?.horaFin ?? ""
-    }
 
     var body: some View {
         NavigationView {
@@ -46,8 +24,6 @@ struct FormularioSolicitudView: View {
                     Text("Solicitante: \(usuario.nombre)")
                     Text("Correo: \(usuario.correo)")
                     Text("Sala: \(sala)")
-                    Text("Fecha: \(fecha)")
-                    Text("Horario: \(horaInicio) - \(horaFin)")
                 }
 
                 Section("Motivo") {
